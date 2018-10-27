dc00768fd2969dc5ee989c6678f402c0d1921e68
Lucky-Hyun <ctuenmuk@uwaterloo.ca>
2018-10-27 19:58:08 -0400

    first draft

--- Test Map/qgis2web_2018_10_27-18_51_47_497822/data/WaterLocations_1.js
+++
@@ -1,9 +0,0 @@
-var json_WaterLocations_1 = {
-"type": "FeatureCollection",
-"name": "WaterLocations_1",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": "1", "Location": "Australia" }, "geometry": { "type": "Point", "coordinates": [ 144.763392285256231, -38.275874525562244 ] } },
-{ "type": "Feature", "properties": { "id": "2", "Location": "Bahamas" }, "geometry": { "type": "Point", "coordinates": [ -77.895593282076632, 24.31621190632541 ] } }
-]
-}
