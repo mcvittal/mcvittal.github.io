b9fb52ca8cc1cc5a29befbfc9f8bf9bd3f8e02a0
Lucky-Hyun <ctuenmuk@uwaterloo.ca>
2018-10-28 01:48:21 -0400

    rev 5 audio with pause testing

--- Test Map/qgis2web_2018_10_27-21_14_21_364066/data/Forest_3.js
+++
@@ -1,9 +0,0 @@
-var json_Forest_3 = {
-"type": "FeatureCollection",
-"name": "Forest_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Location": "Democratic Republic of the Congo", "Play ID": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 23.49, -3.53 ] } },
-{ "type": "Feature", "properties": { "Location": "Russia", "Play ID": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 96.25, 57.59 ] } }
-]
-}
