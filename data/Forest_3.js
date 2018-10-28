dd5b5134b3fb772c29796fdec210f310e3eb7261
Lucky-Hyun <ctuenmuk@uwaterloo.ca>
2018-10-28 18:46:53 -0400

    rev6

--- data/Forest_3.js
+++ data/Forest_3.js
@@ -3,7 +3,7 @@ var json_Forest_3 = {
 "name": "Forest_3",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "Location": "Democratic Republic of the Congo", "Play ID": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 23.49, -3.53 ] } },
-{ "type": "Feature", "properties": { "Location": "Russia", "Play ID": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 96.25, 57.59 ] } }
+{ "type": "Feature", "properties": { "Location": "Democratic Republic of the Congo", "Play ID": 3.0 }, "geometry": { "type": "Point", "coordinates": [ 23.494153937740293, -3.530828041941645 ] } },
+{ "type": "Feature", "properties": { "Location": "Russia", "Play ID": 4.0 }, "geometry": { "type": "Point", "coordinates": [ 96.251607781051604, 57.586018165013165 ] } }
 ]
 }
