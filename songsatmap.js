10234b53f8008b833eac21f13b6ca3f1d6337962
corconut <39305874+corconut@users.noreply.github.com>
2019-02-27 20:12:19 -0500

    Working image expand modal

--- songsatmap.js
+++ songsatmap.js
@@ -7,7 +7,7 @@ function playAudio(src) {
 }
 
 function popUp(feature, layer) {
-    layer.bindPopup('<img class="img-points" src="images/landsat/' + feature.properties.imgname + '"><br><b>Location:</b> ' + feature.properties.name + "<br><b>Classification:</b> " + feature.properties.classification);
+    layer.bindPopup('<a id="landsatimg" href="#landsatModal" data-toggle="modal" data-img-url="images/landsat/' + feature.properties.imgname + '"><img class="img-points" src="images/landsat/' + feature.properties.imgname + '" alt="' + feature.properties.name + '"></a><br><b>Location:</b> ' + feature.properties.name + "<br><b>Classification:</b> " + feature.properties.classification);
 }
 
 var points = new L.GeoJSON.AJAX("data/points.geojson", { onEachFeature: popUp });
@@ -26,9 +26,13 @@ var baseMaps = {
 var songsatmap = L.map('map', {
     center: [44.0, 0],
     zoom: 2,
-    maxBounds: L.latLngBounds([-56,-180], [84,180]),
+    maxBounds: L.latLngBounds([-56, -180], [84, 180]),
     maxBoundsViscosity: 0.75,
     layers: [colourmap, points]
 });
 
+$(document).on("click", "#landsatimg", function(feature, layer){
+    document.getElementById('landsatModalLabel').innerHTML = feature.currentTarget.firstChild.attributes.alt.textContent;
+    document.getElementById('modal-img').innerHTML = '<img class="img-fluid" src="' + feature.currentTarget.dataset.imgUrl + '">';
+ });
 //L.control.layers(baseMaps).addTo(map);
